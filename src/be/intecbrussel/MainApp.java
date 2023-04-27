54264140e9bdb973988f06df905149e9ced0ebdf
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2023-04-27 11:56:43 +0200

    reformating + comments

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -15,38 +15,25 @@ import java.nio.file.Paths;
 
 public class MainApp {
     public static void main(String[] args) {
-        //creating directory
         Path filePath = Paths.get("resources/Secret Folder/");
         try {
+            //creating directory
             if (!filePath.toFile().exists()) {
                 Files.createDirectories(filePath);
             }
-        } catch (IOException e) {
-            System.out.println("An exception occurred when creating the directories\n" + e.getMessage());
-            e.printStackTrace();
-        }
 
-        //creating + writing text to file SECRET MESSAGE.TXT
-        String message = "The wolf is in the pig pen. I repeat: the wolf is in the pig pen.";
-        Path fileName = filePath.resolve("Secret Message.txt");
+            //creating + writing text to file SECRET MESSAGE.TXT
+            try (FileWriter fileWriter = new FileWriter(filePath.resolve("Secret Message.txt").toFile());
+                 BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+                bufferedWriter.write("The wolf is in the pig pen. I repeat: the wolf is in the pig pen.");
+            }
 
-        try (FileWriter fileWriter = new FileWriter(fileName.toFile());
-             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
-            bufferedWriter.write(message);
-        } catch (IOException e) {
-            System.out.println("An exception occurred when writing to the file\n" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        //creating + writing image to IMAGE.JPG
-        Path imageName = filePath.resolve("Secret Picture.jpg");
-        try {
+            //creating + writing image to Secret Picture.jpg
             BufferedImage image = ImageIO.read(new URL("https://worldstories.org.uk/content/book/262/__picture_9352.jpg"));
-            ImageIO.write(image, "jpg", imageName.toFile());
+            ImageIO.write(image, "jpg", filePath.resolve("Secret Picture.jpg").toFile());
         } catch (IOException e) {
-            System.out.println("An exception occurred when handling the image\n" + e.getMessage());
+            System.out.println("An exception occurred\n" + e.getMessage());
             e.printStackTrace();
         }
-
     }
 }
